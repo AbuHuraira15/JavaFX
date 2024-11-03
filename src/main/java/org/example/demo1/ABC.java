23d5802411ccbf48389da808c6186e1bcb898d23
Your Name <you@example.com>
2024-11-04 02:29:13 +0600

    init commit

--- src/main/java/org/example/demo1/ABC.java
+++ src/main/java/org/example/demo1/ABC.java
@@ -12,6 +12,8 @@ import javafx.stage.Stage;              // Top-level JavaFX container (window)
 // Main class that extends Application class to create JavaFX application
 public class ABC extends Application {
 
+
+
     @Override
     public void start(Stage stage) throws Exception {
         Button b = new Button("Click!");  // Creates a new button with text "Click!"
